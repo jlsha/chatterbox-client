ca27c625063f53daaceb3da1e1185dda5a610aef
jlsha <jenniferlaurasha@gmail.com>
2017-01-02 17:34:29 -0800

    added message box and room name box and submit button, working on app.init

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,38 +1,98 @@
-// YOUR CODE HERE:
-var app = {
-  init: () => {
 
-  },
-  
-  send: (message) => {
-    $.ajax({
-  // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/messages',
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (err) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', err);
-      }
-    });
-  },
-  
-  fetch: () => {
-    $.ajax({
-  // This is the url you should use to communicate with the parse API server.
-      type: 'GET',
-      success: function (data) {
-        console.log('chatterbox: Message got', data);
-      },
-      error: function (err) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to get message', err);
-      }
-    });
-  }
+  let app = {
+    init: () => {
+      let getUserName = () => {
+        
+      };
+    },
+    
+    send: (message) => {
+      $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+        url: 'https://api.parse.com/1/classes/messages',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: (data) => {
+          console.log('chatterbox: Message sent', data);
+        },
+        error: (err) => {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message', err);
+        }
+      });
+    },
+    
+    fetch: () => {
+      $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+        type: 'GET',
+        success: (data) => {
+          console.log('chatterbox: Message got', data);
+        },
+        error: (err) => {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to get message', err);
+        }
+      });
+    },
 
-};
+    clearMessages: () => {
+      $('#chats').empty();
+    },
+
+    renderMessage: (message) => {
+      let $message = $('<div></div>');
+      $('#chats').append($message);
+      $message.append('<div class=username>' + message.username + '</div>');
+      $message.append('<div>' + message.text + '</div>');
+    },
+
+    renderRoom: (room) => {
+      $('#roomSelect').append('<div>' + room + '</div>');
+    },
+
+    handleUsernameClick: () => {
+
+    },
+
+    handleSubmit: () => {
+      $('#submit').on('click', function(event) {
+        let message = $('#input-text').val();
+        console.log(message);
+        let roomName = $('#room-text').val();
+        console.log(roomName);
+
+        let messageObj = {
+          username: 'Jen',
+          text: message,
+          roomname: roomName
+        };
+
+        app.send(messageObj);
+      });
+    }
+  };
+
+  app.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
